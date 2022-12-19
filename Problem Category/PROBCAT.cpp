a5d7eeea3bad6ba86e3c2f2f4d6b8f329362d35a
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2022-12-19 17:58:26 +0530

    Create PROBCAT.cpp

---
+++ Problem Category/PROBCAT.cpp
@@ -0,0 +1,46 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll x;
+        cin>>x;
+        if(x<100) cout<<"Easy";
+        else if (x<200) cout<<"Medium";
+        else cout<<"Hard";
+        cout<<endl;
+    }
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
