6834556e3a9fa82e6e55b7d71dd9a39cb1efde6e
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2022-12-19 17:05:56 +0530

    Create CHFRICH.cpp

---
+++ Richie Rich/CHFRICH.cpp
@@ -0,0 +1,44 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll a, b, x, diffAB;
+        cin>>a>>b>>x;
+        diffAB=b-a;
+        cout<<diffAB/x<<endl;
+    }
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
