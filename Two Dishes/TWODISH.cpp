c5506869ae396674deff05bc055c0ddb1f178e41
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2022-12-19 17:48:57 +0530

    Create TWODISH.cpp

---
+++ Two Dishes/TWODISH.cpp
@@ -0,0 +1,45 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll n, a, b, c;
+        cin>>n>>a>>b>>c;
+        if ( (b>=n) && ((a+c)>=n) ) cout<<"Yes";
+        else cout<<"No";
+        cout<<endl;
+    }
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
