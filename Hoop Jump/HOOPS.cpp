df8f9d397afb7cf8107e950b4ff5ce8fbea24661
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2022-12-19 17:13:57 +0530

    Create HOOPS.cpp

---
+++ Hoop Jump/HOOPS.cpp
@@ -0,0 +1,46 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll n;
+        cin>>n;
+        if (n==1) cout<<1;
+        else if (n==3) cout<<2;
+        else cout<<(n/2)+1;
+        cout<<endl;
+    }
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+
+GitHub :
+https://github.com/rushabhvg
+
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
