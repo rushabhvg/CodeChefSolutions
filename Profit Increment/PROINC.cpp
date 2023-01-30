46a8dac8c214b4c47ec78f08454e68f1f98348b7
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-01-30 17:59:56 +0530

    Create PROINC.cpp

---
+++ Profit Increment/PROINC.cpp
@@ -0,0 +1,37 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+int main() {
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll x, y;
+        cin>>x>>y;
+        cout<<y+(0.1*x)<<endl;
+    }
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow
++(0.1*x)https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
