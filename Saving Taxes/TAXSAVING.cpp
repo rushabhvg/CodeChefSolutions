c6b2a1b81e5cfdfcf52d1939ac3db9defc374e1c
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-01-30 17:58:27 +0530

    Create TAXSAVING.cpp

---
+++ Saving Taxes/TAXSAVING.cpp
@@ -0,0 +1,37 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+int main() {
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll x, y;
+        cin>>x>>y;
+        cout<<x-y<<endl;
+    }
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
