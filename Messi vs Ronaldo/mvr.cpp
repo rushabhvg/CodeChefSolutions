0f0364cfdbff2b4a9db51533e71bf49872e9125c
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-07-13 08:12:14 +0530

    Create mvr.cpp

---
+++ Messi vs Ronaldo/mvr.cpp
@@ -0,0 +1,40 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int a, b, x, y;
+    cin>>a>>b>>x>>y;
+    if((2*a)+b == (2*x)+y) {
+        cout<<"Equal";
+    } else if((2*a)+b > (2*x)+y) {
+        cout<<"Messi";
+    } else {
+        cout<<"Ronaldo";
+    }
+    cout<<endl;
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
