f0712914d198faea0f426e53ba40ac6e8197ef95
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-07-19 23:31:04 +0530

    Create PUZHUNT.cpp

---
+++ Puzzle Hunt/PUZHUNT.cpp
@@ -0,0 +1,32 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n>=6 && n<=8) cout<<"YES";
+    else cout<<"NO";
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
