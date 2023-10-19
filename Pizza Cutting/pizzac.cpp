3bbfb3cad3065941a2690ad02acc8e51d388421b
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-10-19 15:46:13 +0530

    Create pizzac.cpp

---
+++ Pizza Cutting/pizzac.cpp
@@ -0,0 +1,39 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll n;
+        cin>>n;
+        if(n==1 || n%2==0) cout<<"YES";
+        else cout<<"NO";
+        cout<<endl;
+    }
+	return 0;
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
