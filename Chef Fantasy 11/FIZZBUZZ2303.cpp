88d121fcfd398fa80462b72d21f8ed892642217e
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2023-11-02 22:52:58 +0530

    Create FIZZBUZZ2303.cpp

---
+++ Chef Fantasy 11/FIZZBUZZ2303.cpp
@@ -0,0 +1,43 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin>>n;
+    cout<<n*(n-1)<<endl;
+    /*
+        A   B   C   D   E   F   G   H   I   J   K
+        
+    */
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long  long  t = 0;cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
