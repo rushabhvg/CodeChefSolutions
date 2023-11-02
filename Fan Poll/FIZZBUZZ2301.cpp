6624ac16eef26a6c42685a281c66ed58864fe7dc
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2023-11-02 22:51:43 +0530

    Create FIZZBUZZ2301.cpp

---
+++ Fan Poll/FIZZBUZZ2301.cpp
@@ -0,0 +1,41 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a, b, c;
+    cin>>a>>b>>c;
+    if(a>b && a>c) cout<<"Yes";
+    else cout<<"No";
+    cout<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // long  long  t = 0;cin>>t;
+    // while (t--){
+        solve();
+    // }
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
