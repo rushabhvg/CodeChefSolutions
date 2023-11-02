2750eaf785723b1d9d628ac237b03db63dcc1bd9
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2023-11-02 22:54:15 +0530

    Create FIZZBUZZ2305.cpp

---
+++ Guess the winner!/FIZZBUZZ2305.cpp
@@ -0,0 +1,40 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin>>n;
+    if(n%2==1 && n!=1) cout<<"Alice"<<endl;
+    else cout<<"Bob"<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 0; cin>>t;
+    while (t--) {
+        solve();
+    }
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
