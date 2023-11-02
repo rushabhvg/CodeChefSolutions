5298bbcab4e5e45a2fe5995f1b1af2fba1bcd188
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2023-11-02 22:52:21 +0530

    Create FIZZBUZZ23_2.cpp

---
+++ Survival Time/FIZZBUZZ23_2.cpp
@@ -0,0 +1,40 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long int n, x, d;
+    cin>>n>>x>>d;
+    int ans = (n/(x*5)) + d;
+    cout<<ans<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long  long  t = 0;cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
