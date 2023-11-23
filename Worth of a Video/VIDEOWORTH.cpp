7e9ca0afdb681fa9c536e1054661e448edee1137
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2023-11-23 15:32:59 +0530

    Create VIDEOWORTH.cpp

---
+++ Worth of a Video/VIDEOWORTH.cpp
@@ -0,0 +1,39 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int s;
+    cin>>s;
+    cout<<s*24*1000<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 0; cin>>t;
+    while (t--) {
+        solve();
+    }
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
