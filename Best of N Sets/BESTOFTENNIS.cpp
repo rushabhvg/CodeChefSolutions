971dedf2cec26e96e0c8e71d0cf2ceeef0555d6d
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2023-11-23 15:34:36 +0530

    Create BESTOFTENNIS.cpp

---
+++ Best of N Sets/BESTOFTENNIS.cpp
@@ -0,0 +1,43 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int x, y;
+    cin>>x>>y;
+    
+    int mx = x;
+    if(mx<y) mx = y;
+    cout<<(mx*2)-1<<endl;
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 0; cin>>t;
+    while (t--) {
+        solve();
+    }
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
