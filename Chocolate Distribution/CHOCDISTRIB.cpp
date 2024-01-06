d0ecf80124d0561c61da83ea1ca4947140ddb22b
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-06 15:13:20 +0530

    Create CHOCDISTRIB.cpp

---
+++ Chocolate Distribution/CHOCDISTRIB.cpp
@@ -0,0 +1,45 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll std::vector<long long int>
+
+void solve() {
+    ll n;
+    cin>>n;
+    if(n==1) {
+        cout<<1<<" "<< 1 <<"\n";
+        return;
+    }
+    cout<<(n+1)/2<<" "<<n<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t = 0; cin>>t;
+    while (t--) {
+        solve();
+    }
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
